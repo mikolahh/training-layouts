30dc86918c6c326027f14ea9207c4c14091b761e
Mikalai Khadanovich <mikolahh@gmail.com>
2023-09-18 17:18:39 +0300

    initialise

---
+++ wow-agency/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('.header-nav__burger').click(function(event) {
+        $('.header-nav__burger, .header-nav__menu, .header-nav__list, .header-nav__item, .header-nav__tel').toggleClass('show-menu');
+       
+    })
+})
